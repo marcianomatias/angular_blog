013161152eb14de732923c7c49ba9ff76751fbdb
marcianomatias <marcianomatias1@hotmail.com>
2023-04-11 15:43:56 -0300

    first commit

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { dataFake } from '../../data/dataFake';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+})
+export class HomeComponent implements OnInit {
+  filmes: any;
+  constructor() {}
+
+  ngOnInit(): void {
+    this.filmes = dataFake;
+  }
+}
